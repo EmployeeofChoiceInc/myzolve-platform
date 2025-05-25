4fb0f5d3516e8b14adfe7205ddc0dd8b6c2ff2c9
Meme Scott <82485727+employeeofchoice@users.noreply.github.com>
2025-05-25 21:39:20 +0000

    Fix: update Firebase config again to see google auth

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -17,12 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
 export const signIn = () => signInWithPopup(auth, provider);
